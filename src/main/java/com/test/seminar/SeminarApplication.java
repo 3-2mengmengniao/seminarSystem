8044e511690111c439bca84ab75d8203ab8bc1a1
wzw <664129552@qq.com>
2018-11-24 21:19:00 +0800

    添加entity、mapper、service文件夹，修改数据库相关配置

--- src/main/java/com/test/seminar/SeminarApplication.java
+++ src/main/java/com/test/seminar/SeminarApplication.java
@@ -1,10 +1,12 @@
 package com.test.seminar;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.*;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan("com.test.seminar.mapper")
 public class SeminarApplication {
 
     public static void main(String[] args) {
