c2e982be768e7dd7d44c2f5473c7d9a7e88869bb
wzw <664129552@qq.com>
2018-11-28 16:14:02 +0800

    新增Dao impl

--- src/main/java/com/test/seminar/dao/CourseDao.java
+++ src/main/java/com/test/seminar/dao/CourseDao.java
@@ -1,17 +1,9 @@
 package com.test.seminar.dao;
 
-import com.test.seminar.mapper.CourseMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 /**
- *
  * @author zhenweiwang
  * @date 2018/11/24
- *
  */
-@Component
-public class CourseDao {
-    @Autowired
-    private CourseMapper courseMapper;
+public interface CourseDao {
 }
---
+++ src/main/java/com/test/seminar/dao/impl/LoginDaoImpl.java
@@ -0,0 +1,29 @@
+package com.test.seminar.dao.impl;
+
+
+import com.test.seminar.dao.LoginDao;
+import com.test.seminar.entity.Student;
+import com.test.seminar.entity.Teacher;
+import com.test.seminar.mapper.LoginMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhenweiwang
+ * @date 2018/11/26
+ */
+@Component
+public class LoginDaoImpl implements LoginDao {
+    @Autowired
+    private LoginMapper loginMapper;
+
+    @Override
+    public Student studentLogin(String account, String password) {
+        return loginMapper.studentLogin(account, password);
+    }
+
+    @Override
+    public Teacher teacherLogin(String account, String password) {
+        return loginMapper.teacherLogin(account, password);
+    }
+}
