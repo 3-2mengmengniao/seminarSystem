3b3a4d25d93ceb60a00b1921bb3ecf630c52fb3e
unknown <742746717@qq.com>
2018-12-01 18:37:16 +0800

    finish serviceImpl

---
+++ src/main/java/com/test/seminar/service/impl/CourseClassServiceImpl.java
@@ -0,0 +1,47 @@
+package com.test.seminar.service.impl;
+
+import com.test.seminar.dao.CourseClassDao;
+import com.test.seminar.entity.CourseClass;
+import com.test.seminar.exception.CourseClassNotFoundException;
+import com.test.seminar.exception.RepetitiveRecordException;
+import com.test.seminar.service.CourseClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class CourseClassServiceImpl implements CourseClassService {
+    @Autowired
+    private CourseClassDao courseClassDao;
+    @Override
+    public CourseClass getCourseClassByCourseClassId(BigInteger courseClassId) throws CourseClassNotFoundException {
+        CourseClass courseClass=courseClassDao.getCourseClassByCourseClassId(courseClassId);
+        if(courseClass==null)
+            throw new CourseClassNotFoundException();
+        return courseClass;
+    }
+
+    @Override
+    public void insertCourseClass(CourseClass courseClass) throws RepetitiveRecordException {
+        courseClassDao.insertCourseClass(courseClass);
+    }
+
+    @Override
+    public void updateCourseClassByCourseClassId(CourseClass courseClass) throws CourseClassNotFoundException {
+        if(courseClassDao.getCourseClassByCourseClassId(courseClass.getId())==null)
+            throw new CourseClassNotFoundException();
+        courseClassDao.updateCourseClassByCourseClassId(courseClass);
+    }
+
+    @Override
+    public void deleteCourseClassByCourseClassId(BigInteger courseClassId) throws CourseClassNotFoundException {
+        if(courseClassDao.getCourseClassByCourseClassId(courseClassId)==null)
+            throw new CourseClassNotFoundException();
+        courseClassDao.deleteCourseClassByCourseClassId(courseClassId);
+    }
+
+    @Override
+    public List<CourseClass> getCourseClassByCourseId(BigInteger courseId) {
+        return courseClassDao.getCourseClassByCourseId(courseId);
+    }
+}
---
+++ src/main/java/com/test/seminar/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,42 @@
+package com.test.seminar.service.impl;
+
+import com.test.seminar.dao.TeacherDao;
+import com.test.seminar.entity.Teacher;
+import com.test.seminar.entity.Teacher;
+import com.test.seminar.exception.RepetitiveRecordException;
+import com.test.seminar.exception.UserNotFoundException;
+import com.test.seminar.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigInteger;
+
+public class TeacherServiceImpl implements TeacherService {
+    @Autowired
+    private TeacherDao teacherDao;
+    @Override
+    public Teacher getTeacherByTeacherId(BigInteger teacherId) throws UserNotFoundException {
+        Teacher teacher=teacherDao.getTeacherByTeacherId(teacherId);
+        if(teacher==null)
+            throw new UserNotFoundException();
+        return teacher;
+    }
+
+    @Override
+    public void insertTeacher(Teacher teacher) throws RepetitiveRecordException {
+        teacherDao.insertTeacher(teacher);
+    }
+
+    @Override
+    public void updateTeacherByTeacherId(Teacher teacher) throws UserNotFoundException {
+        if(teacherDao.getTeacherByTeacherId(teacher.getId())==null)
+            throw new UserNotFoundException();
+        teacherDao.updateTeacherByTeacherId(teacher);
+    }
+
+    @Override
+    public void deleteTeacherByTeacherId(BigInteger teacherId) throws UserNotFoundException {
+        if(teacherDao.getTeacherByTeacherId(teacherId)==null)
+            throw new UserNotFoundException();
+        teacherDao.deleteTeacherByTeacherId(teacherId);
+    }
+}
