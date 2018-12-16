c85177d0a592eac03f756e12d6d5555d125f1723
GillianZhu <24320162202971@stu.xmu.edu.cn>
2018-12-16 11:54:21 +0800

    增加Score实体

---
+++ src/main/java/com/test/seminar/entity/Score.java
@@ -0,0 +1,25 @@
+package com.test.seminar.entity;
+
+
+public class Score {
+    private double presentationScore;
+    private double questionScore;
+    private double reportScore;
+    private double totalScore;
+
+    public double getPresentationScore() { return presentationScore; }
+
+    public void setPresentationScore(double presentationScore) { this.presentationScore = presentationScore; }
+
+    public double getQuestionScore() { return questionScore; }
+
+    public void setQuestionScore(double questionScore) { this.questionScore = questionScore; }
+
+    public double getReportScore() { return reportScore; }
+
+    public void setReportScore(double reportScore) { this.reportScore = reportScore; }
+
+    public double getTotalScore() { return totalScore; }
+
+    public void setTotalScore(double totalScore) { this.totalScore = totalScore; }
+}
