9a83f7de177a10265c3843d770b10dd143cb6d87
jjjkkyz <742746717@qq.com>
2018-12-18 17:37:50 +0800

    uploadStudentExcel 2 transactional

--- src/main/java/com/test/seminar/service/FileService.java
+++ src/main/java/com/test/seminar/service/FileService.java
@@ -1,6 +1,7 @@
 package com.test.seminar.service;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigInteger;
@@ -10,6 +11,7 @@ public interface FileService {
      * 输入Excel文件导入学生，不会读取非excel文件
      * @param file
      */
+    @Transactional
     void uploadStudentExcel(MultipartFile file, BigInteger courseClassId, BigInteger courseId);
 
     MultipartFile downloadFileByFilePath(String filePath);
