23b05efc6688caeb26257adaa073f0be982e23db
GillianZhu <24320162202971@stu.xmu.edu.cn>
2018-12-21 10:14:01 +0800

    添加PresentationDao方法

--- src/main/java/com/test/seminar/dao/PresentationDao.java
+++ src/main/java/com/test/seminar/dao/PresentationDao.java
@@ -1,10 +1,39 @@
 package com.test.seminar.dao;
 
+import com.test.seminar.entity.Presentation;
+
+import java.math.BigInteger;
+import java.util.List;
+
 /**
  * @author cxh
  * @date 2018/11/29
  */
 public interface PresentationDao {
+    /**
+     *
+     * @param seminarControlId
+     * @return
+     */
+    List<Presentation> getPresentationBySeminarControlId(BigInteger seminarControlId);
 
+    /**
+     *
+     * @param presentation
+     * @param courseClassId
+     * @param seminarControlId
+     */
+    void insertPresentation(Presentation presentation, BigInteger courseClassId,BigInteger seminarControlId);
 
+    /**
+     *
+     * @param presentation
+     */
+    void updatePresentation(Presentation presentation);
+
+    /**
+     *
+     * @param presentationId
+     */
+    void deletePresentationByPresentationId(BigInteger presentationId);
 }
