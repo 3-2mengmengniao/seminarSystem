f5d8a7c44d56d36ffbbd117e392508a101857b4b
jjjkkyz <742746717@qq.com>
2018-12-23 12:25:04 +0800

    reconstruct file serve

--- src/main/java/com/test/seminar/service/impl/CourseClassServiceImpl.java
+++ src/main/java/com/test/seminar/service/impl/CourseClassServiceImpl.java
@@ -1,13 +1,18 @@
 package com.test.seminar.service.impl;
 
 import com.test.seminar.dao.CourseClassDao;
+import com.test.seminar.dao.StudentDao;
 import com.test.seminar.entity.CourseClass;
+import com.test.seminar.entity.Student;
 import com.test.seminar.exception.CourseClassNotFoundException;
 import com.test.seminar.exception.RepetitiveRecordException;
 import com.test.seminar.service.CourseClassService;
+import com.test.seminar.util.FileProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -15,6 +20,8 @@ import java.util.List;
 public class CourseClassServiceImpl implements CourseClassService {
     @Autowired
     private CourseClassDao courseClassDao;
+    @Autowired
+    private StudentDao studentDao;
     @Override
     public CourseClass getCourseClassByCourseClassId(BigInteger courseClassId)throws CourseClassNotFoundException {
         return courseClassDao.getCourseClassByCourseClassId(courseClassId);
@@ -45,4 +52,29 @@ public class CourseClassServiceImpl implements CourseClassService {
         return courseClassDao.getCourseClassByStudentIdAndCourseId(studentId,courseId);
     }
 
+    @Override
+    public void uploadStudentExcel(MultipartFile file, BigInteger courseClassId, BigInteger courseId) {
+        //创建处理EXCEL
+        FileProcessor readExcel=new FileProcessor();
+        //解析excel，获取学生信息集合。
+        List<Student> studentList = null;
+        try {
+            studentList = readExcel.getExcelInfo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(studentList != null) {
+            //迭代添加学生信息
+            for(Student student:studentList){
+                Student studentWithId=studentDao.getStudentByAccount(student.getAccount());
+                if(studentWithId==null)
+                {
+                    studentDao.insertStudent(student);
+                    studentWithId=studentDao.getStudentByAccount(student.getAccount());
+                }
+                courseClassDao.insertCourseClassStudentRelation(courseClassId, studentWithId.getId(), courseId);
+            }
+        }
+    }
 }
